af3dfc85899163313ac59f1276f93628b844dd28
Nicolai6120 <Nicolai6120@mail.ru>
2016-09-02 17:35:23 +0300

    contact/views/index replacement with old index view

--- frontend/views/contact/index.php
+++ frontend/views/contact/index.php
@@ -1,35 +1,92 @@
 <?php
 
-use yii\helpers\Html;
-use yii\grid\GridView;
-
 /* @var $this yii\web\View */
-/* @var $searchModel common\models\ContactSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
+/* @var $searchModel common\models\ContactSearch */
 
-$this->title = 'Contacts';
-$this->params['breadcrumbs'][] = $this->title;
+use yii\grid\GridView;
+use yii\data\ActiveDataProvider;
+use yii\grid\ActionColumn;
+use yii\helpers\Html;
+
+$this->title = 'Контакты';
 ?>
-<div class="contact-index">
+<div class="site-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <div class="jumbotron"></div>
 
-    <p>
-        <?= Html::a('Create Contact', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+    <div class="body-content">
+        <div class="row">
+            <div class="col-lg-3">
+                <h2>Контакты</h2>
+            </div>
+        </div>
 
-            'id',
-            'name',
-            'create_date',
-            'creator_ip',
+        <div class="row">
+            <div class="col-lg-8">
 
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
+                <?php
+
+                $dataProvider = new ActiveDataProvider([
+                    'query' => common\models\Contact::find(),
+                    'pagination' => [
+                        'pageSize' => 30,
+                    ],
+                ]);
+
+                \yii\widgets\Pjax::begin([
+                    'enablePushState'=>false
+                ]);
+
+                echo GridView::widget([
+                    'dataProvider' => $dataProvider,
+                    'columns'=>[
+                        //'id',
+                        [
+                            'attribute' => 'name',
+                            'label' => 'Название контакта',
+                            'format' => 'raw',
+                            'value' => function ($data) {
+                                return Html::a($data->name, ['contact/view', 'id' => $data->id]);
+                            },
+                        ],
+                        [
+                            'attribute' => 'create_date',
+                            'label'=>'Добавлен',
+                            'value' => function ($data) {
+                                return Yii::$app->formatter->asDate($data->create_date, 'd MMM Y');
+                            },
+                        ],
+                        [
+                            'class' => ActionColumn::className(),
+                            'template' => '{delete}',
+                            'buttons' => [
+                                'delete' => function ($url, $model) {
+                                    return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, [
+                                        'title' =>'Удалить',
+                                        'data-confirm'=>"Вы действительно хотите удалить этот контакт?",
+                                        'data-pjax'=>'1'
+                                    ]);
+                                },
+                            ],
+
+                            'urlCreator' => function ($action, $model, $key, $index) {
+                                if ($action === 'delete') {
+                                    $url ='/panel/adverts/edition/delete/'.$model->id;
+                                    return $url;
+                                }
+                            }
+                        ]
+                    ],
+                ]);
+
+                \yii\widgets\Pjax::end();
+                ?>
+            </div>
+            <div class="col-lg-3 col-lg-offset-1">
+                <a class="btn btn-default" href="http://www.yiiframework.com/doc/">Добавить контакт</a>
+            </div>
+        </div>
+
+    </div>
 </div>
