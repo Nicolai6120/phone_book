af6c0c4f8931e65e835cce1165bd03f7f22c4080
Nicolai6120 <Nicolai6120@mail.ru>
2016-09-02 14:56:28 +0300

    Contact model generated with gii

---
+++ common/models/Contact.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "contact".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $create_date
+ * @property integer $creator_ip
+ */
+class Contact extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'contact';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'create_date', 'creator_ip'], 'required'],
+            [['create_date', 'creator_ip'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Название',
+            'create_date' => 'Дата создания',
+            'creator_ip' => 'IP создателя',
+        ];
+    }
+}
