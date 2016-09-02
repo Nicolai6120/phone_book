bf7c531a7557ea781458620968de19c7b46fc107
Nicolai6120 <Nicolai6120@mail.ru>
2016-09-02 13:12:08 +0300

    enabled DB component with correct DSN

--- common/config/main.php
+++ common/config/main.php
@@ -2,6 +2,13 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=phone_book',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
