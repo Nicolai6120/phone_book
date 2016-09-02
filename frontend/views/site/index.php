3acc283f6213de3022f2faf6850b9e01c0c8ae4f
Nicolai6120 <Nicolai6120@mail.ru>
2016-09-02 21:29:41 +0300

    2 forms in index and `view` views were added

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,11 +1,13 @@
 <?php
 
 /* @var $this yii\web\View */
+/* @var $model common\models\Contact */
 
 use yii\grid\GridView;
 use yii\data\ActiveDataProvider;
 use yii\grid\ActionColumn;
 use yii\helpers\Html;
+use yii\widgets\ActiveForm;
 
 $this->title = 'Контакты';
 ?>
@@ -21,69 +23,74 @@ $this->title = 'Контакты';
         </div>
 
         <div class="row">
-            <div class="col-lg-8">
+            <div class="col-lg-7">
 
                 <?php
 
-                    $dataProvider = new ActiveDataProvider([
-                        'query' => common\models\Contact::find(),
-                        'pagination' => [
-                            'pageSize' => 30,
+                $dataProvider = new ActiveDataProvider([
+                    'query' => common\models\Contact::find(),
+                    'pagination' => [
+                        'pageSize' => 30,
+                    ],
+                ]);
+
+                \yii\widgets\Pjax::begin([
+                    'enablePushState'=>false
+                ]);
+
+                echo GridView::widget([
+                    'dataProvider' => $dataProvider,
+                    'columns'=>[
+                        //'id',
+                        [
+                            'attribute' => 'name',
+                            'label' => 'Название контакта',
+                            'format' => 'raw',
+                            'value' => function ($data) {
+                                return Html::a($data->name, ['contact/view', 'id' => $data->id], ['data-pjax'=>'0']);
+                            },
                         ],
-                    ]);
-
-                    \yii\widgets\Pjax::begin([
-                        'enablePushState'=>false
-                    ]);
-
-                    echo GridView::widget([
-                        'dataProvider' => $dataProvider,
-                        'columns'=>[
-                            //'id',
-                            [
-                                'attribute' => 'name',
-                                'label' => 'Название контакта',
-                                'format' => 'raw',
-                                'value' => function ($data) {
-                                    return Html::a($data->name, ['contact/view', 'id' => $data->id], ['data-pjax'=>'0']);
+                        [
+                            'attribute' => 'create_date',
+                            'label'=>'Добавлен',
+                            'value' => function ($data) {
+                                return Yii::$app->formatter->asDate($data->create_date, 'd MMM Y');
+                            },
+                        ],
+                        [
+                            'class' => ActionColumn::className(),
+                            'template' => '{delete}',
+                            'buttons' => [
+                                'delete' => function ($url, $model) {
+                                    return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, [
+                                        'title' =>'Удалить',
+                                        'data-confirm'=>"Вы действительно хотите удалить этот контакт?",
+                                        'data-pjax'=>'1'
+                                    ]);
                                 },
                             ],
-                            [
-                                'attribute' => 'create_date',
-                                'label'=>'Добавлен',
-                                'value' => function ($data) {
-                                    return Yii::$app->formatter->asDate($data->create_date, 'd MMM Y');
-                                },
-                            ],
-                            [
-                                'class' => ActionColumn::className(),
-                                'template' => '{delete}',
-                                'buttons' => [
-                                    'delete' => function ($url, $model) {
-                                        return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, [
-                                            'title' =>'Удалить',
-                                            'data-confirm'=>"Вы действительно хотите удалить этот контакт?",
-                                            'data-pjax'=>'1'
-                                        ]);
-                                    },
-                                ],
-                                'contentOptions' => ['class' => 'text-center', 'style' => 'max-width:20px;'],
+                            'contentOptions' => ['class' => 'text-center', 'style' => 'max-width:20px;'],
 
-                                'urlCreator' => function ($action, $model, $key, $index) {
-                                    if ($action === 'delete') {
-                                        $url ='/panel/adverts/edition/delete/'.$model->id;
-                                        return $url;
-                                    }
+                            'urlCreator' => function ($action, $model, $key, $index) {
+                                if ($action === 'delete') {
+                                    $url ='/panel/adverts/edition/delete/'.$model->id;
+                                    return $url;
                                 }
-                            ]
-                        ],
-                    ]);
+                            }
+                        ]
+                    ],
+                ]);
 
-                    \yii\widgets\Pjax::end();
+                \yii\widgets\Pjax::end();
                 ?>
             </div>
             <div class="col-lg-3 col-lg-offset-1">
                 <a class="btn btn-default" href="http://www.yiiframework.com/doc/">Добавить контакт</a>
+
+                <?php $form = ActiveForm::begin(); ?>
+                <?= $form->field($model, 'name') ?>
+                <?= Html::submitButton('Добавить', ['class' => 'btn btn-primary']) ?>
+                <?php ActiveForm::end(); ?>
             </div>
         </div>
 
